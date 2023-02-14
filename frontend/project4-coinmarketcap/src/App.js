5b95658fe01f85907c3a36c474dd8fd48f4a2a70
quan831993 <quan831993@gmail.com>
2023-02-14 17:34:15 +0700

    14/2/2023 quan

--- frontend/project4-coinmarketcap/src/App.js
+++ frontend/project4-coinmarketcap/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LoginPage from './components/LoginPage';
+import Register from './components/Register';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyADD7SOk6lYqXZczh--7rydg7LR4Mj9OYM",
+    authDomain: "project-4-64893.firebaseapp.com",
+    projectId: "project-4-64893",
+    storageBucket: "project-4-64893.appspot.com",
+    messagingSenderId: "265891814289",
+    appId: "1:265891814289:web:15747ca5a313e1442ed830",
+    measurementId: "G-ND7TVZPFRN"
+  };
+
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage auth={auth} signInWithEmailAndPassword={signInWithEmailAndPassword} />} />
+          <Route path="/register" element={<Register auth={auth} createUserWithEmailAndPassword={createUserWithEmailAndPassword} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/project4-coinmarketcap/src/components/LoginPage.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+let LoginPage = (props) => {
+    let { auth, signInWithEmailAndPassword } = props;
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    let loginAuthentication = (auth, email, password) => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log("user", user);
+                toast('Wellcome')
+            })
+            .catch((error) => {
+                console.log(error.message);
+                toast("Email hoặc mật khẩu không chính xác")
+            })
+
+    }
+
+    return (
+        <div className='page'>
+            <div className="loginContainer">
+                <div className='form'>
+                    <div className="title">
+                        <div className="h3">
+                            <Link to="/login" id='login1'>Đăng nhập</Link>
+
+                        </div>
+                        <div className="h3">
+                            <Link to="/register" id='register1'>Đăng ký</Link>
+
+                        </div>
+                    </div>
+                    <form className="userInfor" onSubmit={handleSubmit((data) => {
+                        loginAuthentication(auth, data.email, data.password)
+                        console.log(data);
+                        console.log(auth);
+                        })}>
+                        <div>
+                            <label htmlFor="inputEmail" className="form-label">Địa chỉ email</label>
+                            <input type="email"
+                                id="inputEmail"
+                                className="form-control"
+                                aria-describedby="passwordHelpBlock"
+                                placeholder="Nhập địa chỉ email của bạn ..."
+                                {...register('email', {
+                                    required: "Vui lòng nhập email của bạn"
+                                })}
+                            />
+                            <p className='errMess'>{errors.email?.message}</p>
+                            <ToastContainer/>
+                        </div>
+                        <div>
+                            <label htmlFor="inputPassword5" className="form-label">Password</label>
+                            <input type="password"
+                                id="inputPassword5"
+                                className="form-control"
+                                aria-describedby="passwordHelpBlock"
+                                placeholder="Nhập mật khẩu của bạn ..."
+                                {...register('password', {
+                                    required: "Vui lòng nhập mật khẩu của bạn",
+                                    minLength: {
+                                        value: 7,
+                                        message: "Mật khẩu phải bao gồm ít nhất 7 kí tự"
+                                    }
+                                }
+                                )}
+                            />
+                            <p className='errMess'>{errors.password?.message}</p>
+                        </div>
+
+                        <br></br>
+                        <button type="submit" className="btn btn-primary">Đăng nhập</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage;
